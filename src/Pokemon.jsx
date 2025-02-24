7486438a9a42e7ea4bad808f6f625b9ad8e3001e
arslan <arslanabbas5511@gmail.com>
2025-02-24 17:00:14 +0500

    fetched all data and display it

--- src/Pokemon.jsx
+++ src/Pokemon.jsx
@@ -1,9 +1,55 @@
+import { useState } from "react"
+import { useEffect } from "react"
 
 const Pokemon = () => {
+  const [data, setData] = useState([])
+
+
+  const fetchApi = async () => {
+
+    try {
+      const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=24')
+      const data = await res.json()
+
+     const soloData =  data.results.map(async (p) => {
+        const pokFetch = await fetch(p.url)
+        const abc = await pokFetch.json()
+        return abc
+
+      })
+
+      const finalData = await Promise.all(soloData)
+
+      setData(finalData)
+
+
+    } catch (error) {
+      console.log(error)
+
+    }
+  }
+
+
+  useEffect(() => {
+    fetchApi()
+  }, [])
   return (
-    <div>
-        <h1>Arslan is here dude</h1>
-    </div>
+    <>
+      <section className="container">
+        <header>
+          <h1>Lets Catch Pokemon</h1>
+        </header>
+        <div>
+          <ul className="cards">
+            {
+              data.map((cur) =>{
+                return <li key={cur.id}>{cur.name}</li>
+              })
+            }
+          </ul>
+        </div>
+      </section>
+    </>
   )
 }
 
